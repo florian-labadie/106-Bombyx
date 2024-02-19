71faf9e62c65d488bba45b2134c1659b97fd9c59
Axel <axel.lavrador@epitech.eu>
2024-02-19 17:10:32 +0100

    [FIX] nb of values

--- bombyx.py
+++ bombyx.py
@@ -3,15 +3,15 @@ def bom(n, k):
     return n
 
 def bom1(n, k, i0, i1):
-    while i0 < i1:
+    while i0 <= i1:
         i0 += 1
         n = bom(n, k)
         print(f"{k:.2f} {n:.2f}")
     return n
-            
+
 def bombyx1(n, k):
     n, k, i = float(n), float(k), 1
-    if not 1 <= n <= 100 or not 1 <= k <= 4:
+    if not 1 <= n <= 100 or not 1.0 <= k <= 4.0:
         return 84
     print(f"{i} {n:.2f}")
     while i < 100:
@@ -21,7 +21,7 @@ def bombyx1(n, k):
     return 0
 
 def bombyx2(n, i0, i1):
-    n, i0, i1, k, go = float(n), float(i0), float(i1), 1, 0
+    n, i0, i1, k, go = float(n), float(i0), float(i1), 1.0, 0
     if not 1 <= n <= 100 or not 0 < i0 < i1:
         return 84
     while k <= 4:
@@ -30,5 +30,5 @@ def bombyx2(n, i0, i1):
             go += 1
         n = bom1(n, k, i0, i1)
         k += 0.01
-        go = 1
+        go = 0
     return 0
