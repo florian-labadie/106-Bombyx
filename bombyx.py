eb4cbd57cd092e94ae5a9eb004ca1241854730af
Axel <axel.lavrador@epitech.eu>
2024-02-26 14:23:43 +0100

    [FIX] values

--- bombyx.py
+++ bombyx.py
@@ -7,7 +7,7 @@ def bom1(n, k, i0, i1):
         print(f"{k:.2f} {n:.2f}")
 
 def bombyx1(n, k):
-    n, k = float(n), float(k)
+    n, k = int(n), float(k)
     if not n <= 100 or not (1.0 <= k <= 4.0):
         return 84
     if n < 0:
@@ -19,15 +19,15 @@ def bombyx1(n, k):
     return 0
 
 def bombyx2(n, i0, i1):
-    n, i0, i1 = float(n), int(i0), int(i1)
+    n, i0, i1 = int(n), int(i0), int(i1)
     tmp = n
     if not n <= 100 or not (1 < i0 <= i1):
         return 84
     if n < 0:
         n = 0
     for k in range(100, 401):
-        for _ in range(1, i0):
-            n = bom(n, k/100)
-        bom1(n, k/100, i0, i1)
+        for _ in range(1, i0 - 1):
+            n = bom(n, k / 100)
+        bom1(n, k / 100, i0, i1)
         n = tmp
     return 0
