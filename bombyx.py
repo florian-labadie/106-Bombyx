e8cd8448755c4d48e945b5e2b8e4b18b436d10cb
Florian LABADIE <florian.labadie@epitech.eu>
2024-03-19 11:31:50 +0100

    [FIX] rename fct

--- bombyx.py
+++ bombyx.py
@@ -1,33 +1,33 @@
-def bom(n, k):
+def calc_generation(n, k):
     return (k * n) * (1000 - n) / 1000
 
-def bom1(n, k, i0, i1):
+def further_generation(n, k, i0, i1):
     for i in range(i0, i1 + 1):
-        n = bom(n, k)
+        n = calc_generation(n, k)
         print(f"{k:.2f} {n:.2f}")
 
-def bombyx1(n, k):
+def grown_rate(n, k):
     n, k = int(n), float(k)
     if n < 0 or not (1.0 <= k <= 4.0):
         return 84
     print(f"1 {n:.2f}")
     for i in range(2, 101):
-        n = bom(n, k)
+        n = calc_generation(n, k)
         if n < 0:
             n = 0
         print(f"{i} {n:.2f}")
     return 0
 
-def bombyx2(n, i0, i1):
+def init_and_end_generation(n, i0, i1):
     n, i0, i1 = int(n), int(i0), int(i1)
     tmp = n
     if n < 0 or not (1 < i0 <= i1):
         return 84
     for k in range(100, 401):
         for _ in range(1, i0 - 1):
-            n = bom(n, k / 100)
+            n = calc_generation(n, k / 100)
         if n < 0:
             n = 0
-        bom1(n, k / 100, i0, i1)
+        further_generation(n, k / 100, i0, i1)
         n = tmp
     return 0
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from bombyx import bombyx1, bombyx2
+from bombyx import grown_rate, init_and_end_generation
 import sys
 
 args = sys.argv[1:]
 
 if __name__ == '__main__':
     try:
-        if (len(args) == 2 and bombyx1(args[0], args[1]) == 0):
+        if (len(args) == 2 and grown_rate(args[0], args[1]) == 0):
             exit(0)
-        if (len(args) == 3 and bombyx2(args[0], args[1], args[2]) == 0):
+        if (len(args) == 3 and init_and_end_generation(args[0], args[1], args[2]) == 0):
             exit(0)
         exit(84)
     except Exception as e:
